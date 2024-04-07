e2096a6749fec8e7942be920b9df48554ea71273
Ming-Yang Lu <op8867555@gmail.com>
2024-04-07 16:22:55 +0800

    feat: plantuml extractor

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Optional
 from fastapi import FastAPI, Request, Depends
+from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-
+from marko import Markdown
+from marko.block import FencedCode
 
 from . import models
 from .database import SessionLocal
@@ -20,6 +22,23 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+@app.get("/puml/{note_id}")
+async def puml(note_id: str, db: Session = Depends(get_db)):
+    note = (
+        db.query(models.Note)
+        .where(models.Note.id == models.from_url(note_id))
+        .first()
+    )
+
+    if not note:
+        return 404
+
+    content = next(n for n in Markdown().parse(note.content).children
+                    if isinstance(n, FencedCode))
+    if content:
+        return PlainTextResponse(content.children[0].children)
 
 
 @app.get("/notes")
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, ForeignKey, String, UUID, DateTime
+from sqlalchemy import Column, ForeignKey, String, UUID, DateTime, Uuid
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
-from base64 import urlsafe_b64encode
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 from .database import Base
 
@@ -14,6 +14,7 @@ class Note(Base):
     permission = Column(String)
     ownerId = Column(String, ForeignKey("Users.id"))
     lastchangeAt = Column(DateTime)
+    content = Column(String)
 
     owner = relationship("User", back_populates="notes")
 
@@ -28,3 +29,15 @@ class User(Base):
     id = Column(UUID, primary_key=True)
     email = Column(String)
     notes = relationship("Note", back_populates="owner")
+
+
+def from_url(encoded_id: str):
+    import uuid
+
+    missing_padding = len(encoded_id) % 4
+    if missing_padding:
+        encoded_id += "=" * (4 - missing_padding)
+    try:
+        return uuid.UUID(bytes=urlsafe_b64decode(encoded_id))
+    except Exception:
+        return None
